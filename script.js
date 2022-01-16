32050fb1bb3e60abf53494f345b127a21b9dba8a
IlyasomarH <ilyasomar8857@gmail.com>
2022-01-16 11:55:17 +0300

    introduction

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.getElementById('h1').innerHTML="le lien de script marche ";
+// javaScript and content
+
+document.getElementById('demo').innerHTML="Nous allons apprendre javaScript";
+
+// JavaScript and HTML Attribute Values.
+function Eteindre(argument) {
+	document.getElementById('img').src='../../../sninping tools/javaScript/cours 0/off.png';
+}
+
+function Allumer(argument) {
+	document.getElementById('img').src='../../../sninping tools/javaScript/cours 0/on.png'
+}
+
+
+//JavaScript and HTML Styles (CSS).
+
+document.getElementById('h1').style.color="red";
+
+// JavaScript and Hidding HTML Elements
+
+document.getElementById('img').style.display="none";
+
+
+//JavaScript and Showing HTML Elements
